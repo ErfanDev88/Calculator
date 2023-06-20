0f92b3dc22bc860a7ea7a989ee636e85577e1fae
Mohamad Erfan <130228115+ErfanDev88@users.noreply.github.com>
2023-06-20 17:17:43 +0330

    add react files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Calculator from "./components/Calculator";
+import './calculator.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="title">Calculator</h1>
+      <Calculator />
+    </>
+  );
+}
+
+export default App;
